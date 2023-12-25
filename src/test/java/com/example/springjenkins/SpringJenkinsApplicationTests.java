4499dce77adb48236f894e48d1cb178ef600064f
Max <htpbltyn57@gmail.com>
2023-12-26 00:10:18 +0300

    FirstCommit

--- src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/example/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+
+    @Value("${test.stand}")
+    public String standName;
+
     @Test
     void contextLoads() {
+        System.out.println("HI " + standName);
     }
 
 }
